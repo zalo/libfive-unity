89bd99b5bae35f0f6ff7fa160f9ad7fe61940460
Johnathon Selstad <makeshifted@gmail.com>
2018-09-21 13:49:49 -0700

    Fiz Gizmo Drawing and Picking for Deep Hierarchies

--- Assets/libfive/Scripts/LFShape.cs
+++ Assets/libfive/Scripts/LFShape.cs
@@ -16,8 +16,9 @@ public class LFShape : MonoBehaviour {
   public float boundsSize = 2.5f;
   [Range(0.001f, 180f)]
   public float vertexSplittingAngle = 180f;
-  [Range(10f, 30f)]
-  public float resolution = 15;
+  [Tooltip("Corresponds to Powers of 2")]
+  [Range(4f, 64f)]
+  public float resolution = 8f;
 
   Material defaultMat;
   [System.NonSerialized]
@@ -46,7 +47,7 @@ public class LFShape : MonoBehaviour {
     isRootNode = transform.parent == null || transform.parent.GetComponent<LFShape>() == null;
     if (tree == null || transform.hasChanged || cachedMesh.vertexCount == 0) {
       Evaluate();
-      tree.RenderMesh(cachedMesh, new Bounds(transform.position, Vector3.one * boundsSize), resolution, vertexSplittingAngle);
+      tree.RenderMesh(cachedMesh, new Bounds(transform.position, Vector3.one * boundsSize), resolution + 0.001f, vertexSplittingAngle);
     }
     if (cachedMesh.vertexCount > 0 && defaultMat && isRootNode) {
       Graphics.DrawMesh(cachedMesh, Matrix4x4.identity, defaultMat, gameObject.layer, camera);
@@ -58,13 +59,17 @@ public class LFShape : MonoBehaviour {
 #if UNITY_EDITOR
     if (isSelected != (isSelected = gameObject == Selection.activeGameObject)) transform.hasChanged = true;
     Gizmos.color = new Color(0.368f, 0.466f, 0.607f, 0.251f);
-    if (isSelected && cachedMesh != null && cachedMesh.vertexCount > 0) Gizmos.DrawWireMesh(cachedMesh);
+    if (isSelected && cachedMesh != null && cachedMesh.vertexCount > 0) {
+      if (transform.parent != null && transform.parent.GetComponent<LFShape>()) Gizmos.matrix = transform.parent.localToWorldMatrix;
+      Gizmos.DrawWireCube(transform.position, Vector3.one * boundsSize);
+      Gizmos.DrawWireMesh(cachedMesh);
+    }
 #endif
   }
 
   public LFTree Evaluate() {
     using (LFContext.Active = new Context()) {
-      if (op < LibFive_Operation.Inverse) {
+      if (op < LibFive_Operation.Transform) {
         tree = evaluateNonary(op);
       } else if (op < LibFive_Operation.Union && transform.childCount > 0) {
         LFShape childShape = transform.GetChild(0).GetComponent<LFShape>();
@@ -81,7 +86,7 @@ public class LFShape : MonoBehaviour {
       if (isRootNode && tree != null) {
         tree = LFMath.Transform(tree, transform.localToWorldMatrix);
       } else {
-        tree = LFMath.Transform(tree, transform.parent.localToWorldMatrix.inverse * transform.localToWorldMatrix);
+        tree = LFMath.Transform(tree, transform.parent.worldToLocalMatrix * transform.localToWorldMatrix);
       }
 
       //This prevents this node's tree from being disposed of
@@ -100,30 +105,36 @@ public class LFShape : MonoBehaviour {
     } else if (op == LibFive_Operation.Cylinder) {
       return LFMath.Cylinder(0.5f, 1f, Vector3.back * 0.5f);
     }
-    return LFTree.ConstVar;
+    return tree;
   }
 
   LFTree evaluateUnary(LibFive_Operation op, LFTree tree) {
-    if (op == LibFive_Operation.Inverse) {
-      return -tree;
-    } else if (op == LibFive_Operation.Mirror) {
-      return LFMath.SymmetricX(tree);
-    } else if (op == LibFive_Operation.Shell) {
-      return LFMath.Shell(tree, 0.025f);
+    if (tree != null) {
+      if (op == LibFive_Operation.Transform) {
+        return tree;
+      } else if (op == LibFive_Operation.Inverse) {
+        return -tree;
+      } else if (op == LibFive_Operation.Mirror) {
+        return LFMath.SymmetricX(tree);
+      } else if (op == LibFive_Operation.Shell) {
+        return LFMath.Shell(tree, 0.025f);
+      }
     }
-    return LFTree.ConstVar;
+    return this.tree;
   }
 
   LFTree evaluateNnary(LibFive_Operation op, params LFTree[] trees) {
-    if (trees.Length == 1) return trees[0];
-    if (op == LibFive_Operation.Union) {
-      return LFMath.Union(trees);
-    } else if (op == LibFive_Operation.Intersection) {
-      return LFMath.Intersection(trees);
-    } else if (op == LibFive_Operation.Difference) {
-      return LFMath.Difference(trees);
+    if (trees.Length > 0 || trees[0] == null) {
+      if (trees.Length == 1) return trees[0];
+      if (op == LibFive_Operation.Union) {
+        return LFMath.Union(trees);
+      } else if (op == LibFive_Operation.Intersection) {
+        return LFMath.Intersection(trees);
+      } else if (op == LibFive_Operation.Difference) {
+        return LFMath.Difference(trees);
+      }
     }
-    return LFTree.ConstVar;
+    return tree;
   }
 
   public enum LibFive_Operation {
@@ -133,6 +144,7 @@ public class LFShape : MonoBehaviour {
     Box,
     Cylinder,
     //Unary
+    Transform,
     Inverse,
     Mirror,
     Shell,
@@ -148,9 +160,9 @@ public class LFShape : MonoBehaviour {
 public class LFShapeGizmoDrawer {
   [DrawGizmo(GizmoType.Pickable | GizmoType.NonSelected)]
   static void DrawGizmoForMyScript(LFShape src, GizmoType gizmoType) {
-    Vector3 position = src.transform.position;
     if (src.cachedMesh != null && src.cachedMesh.vertexCount > 0) {
       Gizmos.color = new Color(0f, 0f, 0f, 0f);
+      if (src.transform.parent != null && src.transform.parent.GetComponent<LFShape>()) Gizmos.matrix = src.transform.parent.localToWorldMatrix;
       Gizmos.DrawMesh(src.cachedMesh);
     }
   }
